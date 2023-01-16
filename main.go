5293a967a9da081fcc4a7ead3c76ed13c493c30e
Maxim Petrusenko <maxim.petrusenko@unitb.de>
2023-01-16 20:13:52 +0100

    add main

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"go-blockchain/wallet"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PubliceKeyStr())
+}
